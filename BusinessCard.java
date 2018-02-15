3682287029c016fc8d8f031350cf5a9fa36b80da
russ1001 <30474446+russ1001@users.noreply.github.com>
2018-02-15 07:24:15 -0600

    Add files via upload

---
+++ BusinessCard.java
@@ -0,0 +1,28 @@
+/* 
+ * Russell Streff
+ * CSCI 225
+ * Problem Set #1 - Business Card
+ * 
+ */
+
+// declare the class named BusinessCard
+public class BusinessCard {
+
+	// declare the method named main
+	public static void main(String[] args) {
+		
+		// print to console 
+		
+		System.out.println("Name:  Russell Streff");
+		System.out.println("Cell:  630-484-1001");
+		System.out.println("Email: rstre3@uis.edu");
+			
+
+		
+	}
+
+// end of main method
+	
+}
+
+// end of BusinessCard class
