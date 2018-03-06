f977c83bb96765b0cc7f0f43e0a93ca8e857c5ec
russ1001 <30474446+russ1001@users.noreply.github.com>
2018-03-06 08:39:24 -0600

    Update IntegerSquaredGreaterThanUserInteger.java

--- IntegerSquaredGreaterThanUserInteger.java
+++ IntegerSquaredGreaterThanUserInteger.java
@@ -2,16 +2,21 @@
  * CSCI 225
  * Problem Set #5
  * 
- * 					Problem Statement #6– N2 
+ * 					Problem Statement #6â€“ N2 
  * 
  * 
 Write a program that asks the user for a positive integer.  
 Then, using a while loop, find the smallest integer n such that n2 is greater than the users integer.  
 If the user enters an integer that is not greater than or equal to 0 then display an error. 
+
 Input Format 
 A positive integer supplied by the user. 
+
 Output Format 
-Smallest integer n such that it n2 is greater than the user’s supplied number
+Smallest integer n such that it n2 is greater than the userâ€™s supplied number
+
+
+
  */
 
 
@@ -51,18 +56,23 @@ public class IntegerSquaredGreaterThanUserInteger {
 
 // exit program 						
 			System.exit(1);
-			
+	
 		}
 // close if 
+		
+		
+		
 		
 // while loop iterates as long as the newInteger calculated in the loop is less than the user's entered integer		
 		while(newInteger < userInteger) {
 			
 // increment the counter by one			
-				counter++;
+			
 				
 // set the newInteger variable value to the counter to the power of 2 casting the value to an int as the Math.pow is used for doubles				
-				newInteger = (int) Math.pow(counter, 2);
+			newInteger = (int)Math.pow(counter, 2);
+			
+				counter++;
 				
 			}
 // close while loop		
@@ -71,7 +81,7 @@ public class IntegerSquaredGreaterThanUserInteger {
 		System.out.println("------------------------------------------------");
 		
 // print the answer of the smallest integer n such that n^2 is greater than the entered integer by the user		
-		System.out.println("The smallest integer n such that n^2 is greater than " + userInteger +  ":  " +  counter + "^2 > " + userInteger + ", and " + counter + "^2 is " + newInteger);
+		System.out.println("The smallest integer n such that n^2 is greater than " + userInteger +  " is,  " +  (counter-1) + "^2 > " + userInteger + ", and the squared integer is " + newInteger + ".");
 		
 	}
 // close main method
