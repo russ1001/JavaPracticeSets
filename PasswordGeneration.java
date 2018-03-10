538cf1d6f81afb7d90c344873d9b5dd366d77111
russ1001 <30474446+russ1001@users.noreply.github.com>
2018-03-10 08:54:04 -0600

    Add files via upload

--- PasswordGeneration.java
+++ PasswordGeneration.java
@@ -24,15 +24,10 @@ An n length string containing randomly generated characters.
 */
 
 
-
-
-
-
-
 // import the entire java utility using the * wildcard callout
 import java.util.*;
 
-// import the SecureRandom class for the generation of a secure random number
+// import the SecureRandom class for the generation of a secure random number rather than using Math.random or Random
 import java.security.SecureRandom;
 
 // declare the class PasswordGeneration
@@ -44,55 +39,54 @@ public class PasswordGeneration {
 // declare the Scanner variable input and set it's value to the user entered data via the console		
 		Scanner input = new Scanner(System.in);
 		
-// declare the variable num as an integer
-		int length;
-		int newPass = 0;
-		char finalPass;
-		String ascii = "!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~\r\n" + "";
+// declare the variables length, charValue as integers and initialize their values to zero
+// declare the variable newPass as a character 
+// declare the variable asciiSet as a String and set it's value to the full printable ascii set excluding the 'space' which is the first character of the set
+// declare the variable password as a SecureRandom generated number
+		int length = 0;
+		int charValue = 0;
+		char newPass;	
+		String asciiSet = "!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~\r\n" + "";
 		SecureRandom password = new SecureRandom();
-		
-// prompt the user via the console to enter the integer to be used to help generate the random number
+			
+// prompt the user via the console to enter the integer to be used to set the length of the random number i.e. password to be generated
 		System.out.println("Please enter an integer to be used to as the length of a password generated: ");
 		
-// set the next data entered by user via the console to the input variable num
+// set the next data entered by user via the console to the input variable length
 		length = input.nextInt();
 
+// close Scanner		
 		input.close(); 
 		
+// if statement checking to make sure that the entered integer length is greater than zero
 		if(length<=0) {
 			
+// print error to the console if the entered integer is less than or equal to zero			
 			System.out.println("Error: The value entered is a negative integer");
+			
+// exit program using the System.exit function with the value set to 'true'			
 			System.exit(1); 
 		}
+// close if		
 		
-			for(int i=0; i<length; i++) {
+// print to console the output explaining the generated random password with a dashed line for spacing the input from the output
+		System.out.println("-----------------------------------------------------------------------------------------------");
+		System.out.print("The new secure random generated password with the length of " + length + " that was entered is: ");
 				
-				
-				newPass = password.nextInt();
-
-				finalPass = 
-				
-				
-			}
-		
-			System.out.println("-----------" + newPass);
-		
-		
-		
-		 
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+// for loop iterating from zero through the length entered by user declaring i as an integer and setting it's value to zero
+// increment one each iteration
+		for(int i=0; i<length; i++) {
+			
+// set the integer variable charValue to the SecureRandom generated integer with the upper limit set to 95 
+// so that the the random generated integer is within the range of printable ascii characters listed as the String asciiSet
+			charValue = password.nextInt(95);
+			
+// set the char variable newPass to the String variable asciiSet character at index of the randomly generated integer charValue			
+			newPass = asciiSet.charAt(charValue);
+			
+			System.out.print(newPass);						 
+		}
+// close for loop	
 	}
 // close main method	
 }
