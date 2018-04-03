d78415e23aa9f6220030327ddd361e127a41132e
russ1001 <30474446+russ1001@users.noreply.github.com>
2018-04-03 16:48:54 -0500

    Add files via upload

---
+++ SortAsInserted.java
@@ -0,0 +1,63 @@
+import java.util.*;
+
+public class SortAsInserted {
+
+	public static void main(String[] args) {
+		
+		
+		Scanner input = new Scanner(System.in);
+		System.out.println("Please enter the number of strings to be inserted: ");
+		int noOfStrings = input.nextInt();
+		
+		String[] stringArrayFromUser = new String[noOfStrings];
+		
+		
+		
+		GetStringsFromUser();
+		
+		
+		
+		
+
+// close main method
+}
+	
+	
+
+		
+
+	
+// insert strings into a string array based on user input InsertStringFromUser method	
+	public static String[] GetStringsFromUser(String list) {
+		
+		
+		
+		String[] stringList = new String[noOfStrings];
+		
+		
+		
+		
+		
+		
+		return stringList;
+		
+		
+	}
+	
+	
+	
+	
+	
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+// close class SortAsInserted
