9d5fb08e405d84a7cddd7fd1b8b4763455a862f8
russ1001 <30474446+russ1001@users.noreply.github.com>
2018-04-15 20:51:52 -0500

    Update SortAsInserted.java

--- SortAsInserted.java
+++ SortAsInserted.java
@@ -1,113 +1,164 @@
-/*Russell Streff
- * CSCI 225
- * 
- * 				Problem Set #7
- * 
-				Problem  #1
-				
-				
-For this problem, do NOT use any class that manipulates data in a collection like the Arrays class or the Collection
-class
-Problem Statement – Sort as Inserted
 
-Write a program that accepts as its first input a number that represents the number of strings to be
-inserted followed by the Strings. 
-The program should insert the strings into an array in its sorted position.
-The array should NOT be sorted after all elements are inserted rather it should be sorted after each individual
-element is inserted. 
-Once all elements have been inserted print out the sorted array. Sort in ASCENDING
-order (A-Z).
-
-You should have methods that do the following:
-Prints the array
-Places element in its sorted position
-
-Input Format
-First input is the number of elements to be inserted.
-On each new line is a string to be inserted.
-
-Output Format
-A single line with the elements printed in ascending order separated by a comma. 
- * 
- * 
- */
-
-
-
-
+// import the entire java utility using the * wildcard callout
 import java.util.*;
 
-public class SortAsInserted { 
+// declare the class SortAsInserted
+public class TEST { 
 
+// declare the main method	
 	public static void main(String[] args) {
 		
-		
+// declare the Scanner variable input and set it to the entered data from keyboard		
 		Scanner input = new Scanner(System.in);
-		System.out.println("Please enter the number of strings to be inserted: ");
+		
+// print to console and prompt user to enter the number of strings to be sorted		
+		System.out.println("Please enter the number of strings to be sorted: ");
+		
+// declare the integer variable length and set it's value to the next inputted value from the user 		
 		int length = input.nextInt();
 		
-		String entry = ""; 
-		 
-
-		for(int i=0; i<length; i++) {
+		if(length<1) {
 			
-			 
-			System.out.println("Enter string #" + (i+1));
-			
-			entry = input.next();
-			
-			sortArray(entry,length);
-		} 
-		
-		
-		
-		
+			System.out.println("Error: You entered a non-positive number");
+			System.exit(1); 
+		}
+	
+// call the sortArray method and pass the size of the array entered by the user		
+		sortArray(length);
+	
+		System.out.println("\nAll done, back to main method");
 		} 
 // close main method		
-
-
 	
-// sort array	
-	public static void sortArray(String x, int num) {
+	
+	
+	
+	
+// sort array method named sortArray
+	public static void sortArray(int userLength){
 		
-		String[] names = new String[num];
-			
+		Scanner in = new Scanner(System.in);
+		String[] methodArray = new String[userLength];
+		String a;
+		String b;
+		String temp="";
+		int i = 0;
 		
-		for(int i=0; i<num; i++) {
-			
-			
-			
-			names[i] = x;
-			
-		}
 		
-		printArray(names, num);
-		
+// WHILE		
+			while(i<userLength) {
+				
+// LENGTH OF 1				
+				if(userLength==1) {	
+				System.out.println("Enter a string: ");
+				a = in.next();
+				methodArray[i] = a;
+				printArray(methodArray);
+				return; 
+			}
+			
+// LENGTH OF 2
+				if(userLength==2) {
+				System.out.println("Enter a string: ");
+				a = in.next();
+							
+				System.out.println("Enter a string: ");
+				b = in.next();
+				
+// compare to sort for array of size 2			
+			if(b.compareTo(a)<0) {
+					 
+					temp = a;
+					a = b;
+					b = temp;
+					
+					methodArray[i] = a;
+					methodArray[i+1] = b; 
+					printArray(methodArray);
+					return;
+			}
+				methodArray[i] = a;
+				methodArray[i+1] = b;
+				printArray(methodArray);
+				return; 
+				 
+			}	
+// close if statement		
+
+				
+// LENGTH OF 3 OR MORE				
+			
+
+				String currentEntry = "";
+				int currentIndex = 0;
+				System.out.println("Enter a string: ");
+				currentEntry = in.next();
+				 
+				while(currentIndex<userLength) {
+				for(i=0; i<userLength; i++) {
+					
+					
+					if(userLength==1) {
+						methodArray[i] = currentEntry;
+						printArray(methodArray);
+						return;
+					}
+					
+					for(int j=i+1; j<userLength; j++) {
+						
+					System.out.println("Enter a string: ");
+					currentEntry = in.next();	
+						
+// keep on getting an error on this comparison??
+					if(currentEntry.compareTo(methodArray[i])<0) {
+						
+						temp = methodArray[i];
+						currentEntry = methodArray[i];
+						methodArray[i] = temp;
+						
+						
+					}
+					
+					currentEntry = methodArray[i+1];
+					
+				} 
+					
+			}
+
+		}	
+
 	}
+// while loop within the range of the array length		
+		return;
+// return to main method
 	
-	
-	
-	
-// print array	
-	public static void printArray(String[] printArray, int sizeOfArray) {
-		
-		for(int i=0; i<sizeOfArray; i++) {
-			
-			
-			
-			System.out.println(printArray[i]);
-			
-		
-		}
-		
-		
-		
 	}
-	
-	
-	
+// close sortArray method	
 	
 
+
+
+// print array method named printArray	
+	public static void printArray(String[] x){
+		
+// for loop to print out array using the length of the string array passed to the method as the upper range limit
+// the string array is passed from the sortArray method		
+			for(int i=0; i<x.length; i++) {
+		
+// if statement checking the count of i in relation to the length of the string array
+// if the increment integer i is equals to the length of the string array minus one 
+// then print out the value of the string array at index i
+// else print out the value of the string array at index i with a comma after the value
+				if(i==x.length-1) {
+				System.out.print(x[i]);
+				}
+				else 
+					System.out.print(x[i] + ", ");
+			}
+// close for loop			
+		
+		}
+// close printArray method	
 }	
 // close class SortAsInserted
 
